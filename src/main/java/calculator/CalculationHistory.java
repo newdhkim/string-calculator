01792a08ef057c9249baf4ec6a9628aa41757ce4
Donghyun Kim <newdhkim0615@gmail.com>
2026-03-19 01:10:06 +0900

    Feat: CalculationHistory 구현
    연산 이력 관리를 담당하는 클래스 구현
    
    - List<String>을 직접 사용하는 대신 History 일급 컬렉션을 필드로 사용
    - 입력값과 결과를 "입력 : {input} => 출력 : {result}" 형식으로 저장

--- src/main/java/calculator/CalculationHistory.java
+++ src/main/java/calculator/CalculationHistory.java
@@ -1,9 +1,22 @@
 package calculator;
 
+import java.util.List;
+
 /**
  * 힌트: 연산 이력을 관리하는 클래스.
  * 일급 컬렉션으로 구현해볼 것.
  * 반드시 이 클래스를 사용할 필요는 없다. 자유롭게 설계할 것.
  */
 public class CalculationHistory {
+
+    private final History history = new History();
+
+    public void save(String input, String result) {
+        String str = "입력 : " + input + " => 출력 : " + result;
+        history.push(str);
+    }
+
+    public List<String> get() {
+        return history.toList();
+    }
 }
